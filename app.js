b8575f63e1e1f0c56381181c71296dcff85af277
Henry Doan <henrydoan96@gmail.com>
2017-05-14 21:23:16 -0600

    add an end game and increase ball velocity in intervals

--- app.js
+++ app.js
@@ -12,20 +12,43 @@ xVelocity = yVelocity = 4;
 
 // Score
 score1 = score2 = 0;
+winScore = 10;
+
+showingWinScreen = false;
 
 // The AI
-aiSpeed = 2;
+aiSpeed = 3;
 
 window.onload = function() {
 	canvas = document.getElementById('gameConsole');
 	canvasContent = canvas.getContext('2d');
 	setInterval(update, 1000 / 30);
+
+	canvas.addEventListener('mousedown', function(e) {
+		if(showingWinScreen) {
+			score1 = 0;
+			score2 = 0;
+			showingWinScreen = false;
+		}
+	});
+
 	canvas.addEventListener('mousemove', function(e) {
 		paddle1y = e.clientY - paddleHeight / 2;
 	});
 }
 
 function reset() {
+	if(score1 >= winScore ||
+		score2 >= winScore) {
+
+		showingWinScreen = true;
+	}
+
+	if(score1 % 3 == 0 ) {
+
+		xVelocity += 2;
+	}
+
 	ballX = canvas.width / 2;
 	ballY = canvas.height / 2;
 	xVelocity =- xVelocity;
@@ -80,11 +103,24 @@ function update() {
 	// screen size
 	canvasContent.fillRect(0, 0, canvas.width, canvas.height);
 
+	if(showingWinScreen) {
+		canvasContent.fillStyle = 'white';
+
+		if(score1 >= winScore) {
+			canvasContent.fillText("You Won!", 350, 200);
+		} else if(score2 >= winScore) {
+			canvasContent.fillText("Computer Won!", 340, 200);
+		}
+		canvasContent.fillText("click to restart", 340, canvas.height / 2);
+		
+		return;
+	}
+
 	canvasContent.fillStyle = 'white';
 
 	// Net
 	for(var i = 0; i < canvas.height; i += 40) {
-		canvasContent.fillRect(canvas.width/2-1, i, 6, 20);
+		canvasContent.fillRect(canvas.width / 2 - 1, i, 6, 20);
 	}
 
 	// Paddles
